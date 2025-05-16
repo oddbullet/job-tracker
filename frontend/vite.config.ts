d7262fbbd835547a8d152f1810053faeaae449d2
Bill Yang <joyang12345@gmail.com>
2025-05-16 15:45:25 -0400

    Setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
+  },
   plugins: [vue()],
 })
+
